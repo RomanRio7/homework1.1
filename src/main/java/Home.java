27d6adb7ed98dfacb31e0fbaf9afac12342c11d0
Roman <roman_sarsengaliyev@mail.ru>
2025-06-06 22:37:03 +0300

    homework 1

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -8,9 +8,9 @@ public class Home {
         for (int i = 1; i <= n; i++) {
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 System.out.println("FizzBuzz");
-            } else if ((i % 3 == 0) && (i % 5 != 0)) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if ((i % 3 != 0) && (i % 5 == 0)) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
